6a0828c9faec53412da628836a5aebbb771e3726
bocsir <jacobwharris13@gmail.com>
2024-01-18 02:25:55 -0500

    ugly todo

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 //importing needed functions from respective software dev kit (SDK)
 //import { <service getter functions> } from "<content delivery network (CDN) URL>";
 import { initializeApp, getApps} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 //firebase config for this web app (from firebase console)
@@ -63,8 +63,6 @@ document.getElementById('google-login').addEventListener('click', () => {
     });
 });
 
-//need to setup register and login forms in html using <form> and a submit button with type="submit"
-
 // //register form
 document.getElementById('sign-up-form').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -121,6 +119,70 @@ document.getElementById('sign-up').addEventListener('click', () => {
 
 
 });
+
+//todo list
+const form = document.getElementById("new-task-form");
+const input = document.getElementById("new-task-input");
+const listElement = document.getElementById("tasks");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = input.value;
+
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskContentElement = document.createElement("div");
+    taskContentElement.classList.add("content");
+
+    taskElement.appendChild(taskContentElement);
+
+    const taskInputElement = document.createElement("input");
+    taskInputElement.classList.add("text");
+    taskInputElement.type = "text";
+    taskInputElement.value = task;
+    taskInputElement.setAttribute("readonly", "readonly");
+
+    taskContentElement.appendChild(taskInputElement);
+
+    const taskActionsElement = document.createElement("div");
+    taskActionsElement.classList.add("actions");
+    
+    const taskEditElement = document.createElement("button");
+    taskEditElement.classList.add("edit");
+    taskEditElement.innerText = "Edit";
+
+    const taskDeleteElement = document.createElement("button");
+    taskDeleteElement.classList.add("delete");
+    taskDeleteElement.innerText = "Delete";
+
+    taskActionsElement.appendChild(taskEditElement);
+    taskActionsElement.appendChild(taskDeleteElement);
+
+    taskElement.appendChild(taskActionsElement);
+
+    listElement.appendChild(taskElement);
+
+    input.value = "";
+
+    taskEditElement.addEventListener("click", (e) => {
+        if (taskEditElement.innerText.toLowerCase() == "edit") {
+            taskEditElement.innerText = "Save";
+            taskInputElement.removeAttribute("readonly");
+            taskInputElement.focus();
+
+        } else {
+            taskEditElement.innerText = "Edit";
+            taskInputElement.setAttribute("readonly", "readonly");
+        }
+    });
+
+    taskDeleteElement.addEventListener("click", (e) => {
+        listElement.removeChild(taskElement);
+    })
+})
+
 
 //begin with zero values in clock
 document.getElementById('hours').value = '00';
